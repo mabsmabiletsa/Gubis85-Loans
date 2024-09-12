8023ddbe98f2ee7d01052af4d95f12072e659a10
mabsmabiletsa <107240407+mabsmabiletsa@users.noreply.github.com>
2024-09-12 09:03:20 +0200

    9122024-1

--- script.js
+++ script.js
@@ -11,6 +11,48 @@ const employees = [
             { amount: 44850, remainingInstallments: 5, installmentAmount: 5606, paid: 16818, balance: 28032 }
         ]
     },
+    {
+        name: "Ishmael Ncgobo",
+        loans: [
+            { amount: 20000, remainingInstallments: 3, installmentAmount: 3333.33, paid: 9999.99, balance: 10000.01}
+        ]
+    },
+    {
+        name: "Luzuko Komani",
+        loans: [
+            { amount: 5000, remainingInstallments: 2, installmentAmount: 1000, paid: 3000, balance: 2000 }
+        ]
+    },
+    {
+        name: "Sipho Gumede",
+        loans: [
+            { amount: 20000, remainingInstallments: 1, installmentAmount: 5000, paid: 15000, balance: 5000 }
+        ]
+    },
+    {
+        name: "Thokozani Mahlangu",
+        loans: [
+            { amount: 63000, remainingInstallments: 12, installmentAmount: 5000, paid: 5000, balance: 58000 }
+        ]
+    },
+    {
+        name: "Tshwarelo Ndala",
+        loans: [
+            { amount: 12850, remainingInstallments: 1, installmentAmount: 3350, paid: 10050, balance: 2800 }
+        ]
+    },
+    {
+        name: "William Moshupye",
+        loans: [
+            { amount: 50000, remainingInstallments: 2, installmentAmount: 10000, paid: 31000, balance: 19000 }
+        ]
+    },
+    {
+        name: "Nomvula Nkosi",
+        loans: [
+            { amount: 1000, remainingInstallments: 1, installmentAmount: 500, paid: 500, balance: 500 }
+        ]
+    },
    
     // Add more employees as needed
 ];
